cad2b3682a6031f5076dc14207a54cbcbe07abf3
David Wu <david.wuu@gmail.com>
2021-12-25 15:21:21 -0800

    tmp

--- src/app/common/data-mapper.ts
+++ src/app/common/data-mapper.ts
@@ -1,13 +1,50 @@
 
+[
+	[5,1,2,3],
+	[5,1,2,3],
+	[5,1,2,3],
+]
 
+[]
 
-enum DataOperations {
+enum TransformFunc {
 	CLIP_SPOTTY_DATA = 'CLIP_SPOTTY_DATA';
+	SUBTRACT = 'SUBTRACT';
 }
 
 
-class DataOperator {
+class DataTransform {
+	static transform(transforms, dataByName) {
 
-	
+	}
 }
+
+
+
+
+[
+	{
+		name: 'root',
+		transform: TransformFunc.SUBTRACT,
+		transformParams: {
+		},
+		args: [
+			'clipped',
+			'data2',
+		],
+	},
+	{
+		name: 'clipped',
+		transform: TransformFunc.CLIP_SPOTTY_DATA,
+		transformParams: {
+		},
+		args: [
+			'data1',
+			'data2',
+		],
+	},
+]
+
+
+
 
